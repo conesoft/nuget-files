d2951ae0a53850a357164b4d2e7bd426b7e786bd
David Spörri <davepermen@hotmail.com>
2021-03-16 16:57:55 +0100

    little more..

--- src/Conesoft.Files/Filename.cs
+++ src/Conesoft.Files/Filename.cs
@@ -5,10 +5,11 @@
         public Filename(string name, string extension)
         {
             Name = name;
-            Extension = extension;
+            Extension = extension.StartsWith(".") ? extension.Substring(1) : extension;
         }
 
-        public static Filename From(string name, string extension) => new Filename(name, extension);
+        public static Filename From(string name, string extension) => new(name, extension);
+        public static Filename FromExtended(string nameWithExtension) => new(System.IO.Path.GetFileNameWithoutExtension(nameWithExtension), System.IO.Path.GetExtension(nameWithExtension));
 
         public string Name { get; }
         public string Extension { get; }
