e31964e6242d6a73536ad84e0e60af3828cac617
David Spörri <davepermen@hotmail.com>
2021-05-20 22:48:34 +0200

    cleaned up

--- src/Conesoft.Files/BatchHandlerExtensions.cs
+++ src/Conesoft.Files/BatchHandlerExtensions.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
+using IO = System.IO;
 
 namespace Conesoft.Files
 {
@@ -23,6 +24,27 @@ namespace Conesoft.Files
         public static async Task<FileIncludingContent<byte[]>[]> ReadBytes(this IEnumerable<File> files)
             => await Task.WhenAll(files.Select(async file => new FileIncludingContent<byte[]>(file, await file.ReadBytes())));
 
+
+
+        public static async IAsyncEnumerable<File[]> Live(this Directory directory, string? filter = null)
+        {
+            var fw = new IO.FileSystemWatcher(directory.Path, filter ?? "*");
+            var tcs = new TaskCompletionSource();
+
+            fw.Created += (_, e) => tcs.SetResult();
+            fw.Renamed += (_, e) => tcs.SetResult();
+            fw.Changed += (_, e) => tcs.SetResult();
+            fw.Deleted += (_, e) => tcs.SetResult();
+            fw.EnableRaisingEvents = true;
+
+            while (true)
+            {
+                yield return directory.Filtered(filter ?? "*", allDirectories: false).ToArray();
+                await tcs.Task;
+                tcs = new();
+            }
+        }
+
         public static async IAsyncEnumerable<(File[] All, File[] Changed, File[] Added, File[] Deleted)> Changes(this IAsyncEnumerable<File[]> liveFiles)
         {
             Dictionary<File, DateTime> lastModified = new();
@@ -31,24 +53,22 @@ namespace Conesoft.Files
             {
                 var added = files.Except(lastModified.Keys).ToArray();
                 var deleted = lastModified.Keys.Except(files).ToArray();
-
                 var changed = files.Except(added).Where(f => lastModified[f] < f.Info.LastWriteTime).ToArray();
 
-                foreach (var file in changed.Concat(added))
-                {
-                    lastModified[file] = file.Info.LastWriteTime;
-                }
+                lastModified = files.ToDictionaryValues(file => file.Info.LastWriteTime);
 
                 if ((changed.Length | added.Length | deleted.Length) > 0)
                 {
                     yield return (
                         All: files,
-                        Changed: changed.ToArray(),
-                        Added: added.ToArray(),
+                        Changed: changed,
+                        Added: added,
                         Deleted: deleted
                     );
                 }
             }
         }
+
+        public static Dictionary<TKey, TValue> ToDictionaryValues<TKey, TValue>(this IEnumerable<TKey> keys, Func<TKey, TValue> valueGenerator) where TKey : notnull => keys.ToDictionary(key => key, valueGenerator);
     }
 }
