2e5717c7538ba87a740faa943ad71261dbf007c6
David Spörri <davepermen@hotmail.com>
2021-06-09 13:30:57 +0200

    yay

--- src/Conesoft.Files/BatchHandlerExtensions.cs
+++ src/Conesoft.Files/BatchHandlerExtensions.cs
@@ -24,17 +24,19 @@ namespace Conesoft.Files
         public static async Task<FileIncludingContent<byte[]>[]> ReadBytes(this IEnumerable<File> files)
             => await Task.WhenAll(files.Select(async file => new FileIncludingContent<byte[]>(file, await file.ReadBytes())));
 
-        public static async IAsyncEnumerable<File[]> Live(this Directory directory, string? filter = null)
+        public static async IAsyncEnumerable<File[]> Live(this Directory directory, string? filter = null, int timeout = 2500)
         {
             var fw = new IO.FileSystemWatcher(directory.Path, filter ?? "*");
             var tcs = new TaskCompletionSource<File[]>();
+            bool timedout = false;
 
             _ = Task.Run(() =>
             {
                 while(true)
                 {
-                    tcs.SetResult(directory.Filtered(filter ?? "*", allDirectories: false).ToArray());
-                    fw.WaitForChanged(IO.WatcherChangeTypes.All);
+                    tcs.TrySetResult(directory.Filtered(filter ?? "*", allDirectories: false).ToArray());
+                    var result = fw.WaitForChanged(IO.WatcherChangeTypes.All, timedout ? timeout : timeout / 10);
+                    timedout = result.TimedOut;
                 }
             });
 
@@ -51,13 +53,14 @@ namespace Conesoft.Files
 
             await foreach (var files in liveFiles)
             {
+                Console.WriteLine($"Changed() - {string.Join(", ", files.Select(f => f.NameWithoutExtension).ToArray())}");
                 var added = files.Except(lastModified.Keys).ToArray();
                 var deleted = lastModified.Keys.Except(files).ToArray();
                 var changed = files.Except(added).Where(f => lastModified[f] < f.Info.LastWriteTime).ToArray();
 
                 lastModified = files.ToDictionaryValues(file => file.Info.LastWriteTime);
 
-                if ((changed.Length | added.Length | deleted.Length) > 0)
+                //if ((changed.Length | added.Length | deleted.Length) > 0)
                 {
                     yield return (
                         All: files,
