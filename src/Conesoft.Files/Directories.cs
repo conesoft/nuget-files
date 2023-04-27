f2b054f1af221b89ef60a1513fec10c8ec5e7d0a
David Spörri <davepermen@hotmail.com>
2023-04-27 21:09:02 +0200

    ok go

--- src/Conesoft.Files/Directories.cs
+++ src/Conesoft.Files/Directories.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 using E = System.Environment;
 
 namespace Conesoft.Files
@@ -9,9 +11,9 @@ namespace Conesoft.Files
     {
         static internal Directory FromSpecial(E.SpecialFolder folder) => Directory.From(E.GetFolderPath(folder));
 
-        public Directory Current => Directory.From(E.CurrentDirectory);
+        public static Directory Current => Directory.From(E.CurrentDirectory);
 
-        public TempDirectory Temporary => new();
+        public static TempDirectory Temporary => new();
 
         public UserDirectories User { get; } = new UserDirectories();
 
@@ -21,12 +23,18 @@ namespace Conesoft.Files
     [EditorBrowsable(EditorBrowsableState.Never)]
     public class UserDirectories
     {
-        public Directory Local => Directories.FromSpecial(E.SpecialFolder.LocalApplicationData);
-        public Directory Roaming => Directories.FromSpecial(E.SpecialFolder.ApplicationData);
-        public Directory Documents => Directories.FromSpecial(E.SpecialFolder.MyDocuments);
-        public Directory Pictures => Directories.FromSpecial(E.SpecialFolder.MyPictures);
-        public Directory Music => Directories.FromSpecial(E.SpecialFolder.MyMusic);
-        public Directory Videos => Directories.FromSpecial(E.SpecialFolder.MyVideos);
-        public Directory Desktop => Directories.FromSpecial(E.SpecialFolder.DesktopDirectory);
+        public static Directory Local => Directories.FromSpecial(E.SpecialFolder.LocalApplicationData);
+        public static Directory Roaming => Directories.FromSpecial(E.SpecialFolder.ApplicationData);
+        public static Directory Documents => Directories.FromSpecial(E.SpecialFolder.MyDocuments);
+        public static Directory Pictures => Directories.FromSpecial(E.SpecialFolder.MyPictures);
+        public static Directory Music => Directories.FromSpecial(E.SpecialFolder.MyMusic);
+        public static Directory Videos => Directories.FromSpecial(E.SpecialFolder.MyVideos);
+        public static Directory Desktop => Directories.FromSpecial(E.SpecialFolder.DesktopDirectory);
+        public static Directory Downloads => Directory.From(SHGetKnownFolderPath(new Guid("374DE290-123F-4565-9164-39C4925E467B"), 0));
+
+
+        [DllImport("shell32", CharSet = CharSet.Unicode, ExactSpelling = true, PreserveSig = false)]
+        private static extern string SHGetKnownFolderPath([MarshalAs(UnmanagedType.LPStruct)] Guid rfid, uint dwFlags, nint hToken = default);
+
     }
 }
