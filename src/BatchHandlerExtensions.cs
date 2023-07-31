7815ac41316f6790aba72519a82aab02945a8087
David Spörri <davepermen@hotmail.com>
2023-07-31 16:15:13 +0200

    tiny improvements

--- src/BatchHandlerExtensions.cs
+++ src/BatchHandlerExtensions.cs
@@ -13,16 +13,19 @@ namespace Conesoft.Files
     public static class BatchHandlerExtensions
     {
         public static async Task<FileIncludingContent<T>[]> ReadFromJson<T>(this IEnumerable<File> files, JsonSerializerOptions? options = null)
-            => await Task.WhenAll(files.Select(async file => new FileIncludingContent<T>(file, await file.ReadFromJson<T>(options))));
+            => await Task.WhenAll(files.Select(async file => new FileIncludingContentMaybe<T>(file, await file.ReadFromJson<T>(options)))).WithContent();
 
         public static async Task<FileIncludingContent<string>[]> ReadText(this IEnumerable<File> files)
-            => await Task.WhenAll(files.Select(async file => new FileIncludingContent<string>(file, await file.ReadText())));
+            => await Task.WhenAll(files.Select(async file => new FileIncludingContentMaybe<string>(file, await file.ReadText()))).WithContent();
 
         public static async Task<FileIncludingContent<string[]>[]> ReadLines(this IEnumerable<File> files)
-            => await Task.WhenAll(files.Select(async file => new FileIncludingContent<string[]>(file, await file.ReadLines())));
+            => await Task.WhenAll(files.Select(async file => new FileIncludingContentMaybe<string[]>(file, await file.ReadLines()))).WithContent();
 
         public static async Task<FileIncludingContent<byte[]>[]> ReadBytes(this IEnumerable<File> files)
-            => await Task.WhenAll(files.Select(async file => new FileIncludingContent<byte[]>(file, await file.ReadBytes())));
+            => await Task.WhenAll(files.Select(async file => new FileIncludingContentMaybe<byte[]>(file, await file.ReadBytes()))).WithContent();
+
+        static async Task<FileIncludingContent<T>[]> WithContent<T>(this Task<FileIncludingContentMaybe<T>[]> files)
+            => (await files).Where(f => f.ContentMaybe != null).Select(f => new FileIncludingContent<T>(f, f.ContentMaybe!)).ToArray();
 
         public static async IAsyncEnumerable<File[]> Live(this Directory directory, bool allDirectories = false, int timeout = 2500)
         {
--- src/FileIncludingContent.cs
+++ src/FileIncludingContent.cs
@@ -6,11 +6,21 @@ namespace Conesoft.Files
     [EditorBrowsable(EditorBrowsableState.Never)]
     public record FileIncludingContent<T> : File
     {
-        public FileIncludingContent(File file, T? content) : base(file)
+        public FileIncludingContent(File file, T content) : base(file)
         {
             this.Content = content;
         }
 
-        public T? Content { get; set; }
+        public T Content { get; set; }
+    }
+
+    public record FileIncludingContentMaybe<T> : File
+    {
+        public FileIncludingContentMaybe(File file, T? contentMaybe) : base(file)
+        {
+            this.ContentMaybe = contentMaybe;
+        }
+
+        public T? ContentMaybe { get; set; }
     }
 }
