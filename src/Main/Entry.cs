d63ccb436d4607d415429241fd4dbad44106443d
David Spörri <davepermen@hotmail.com>
2025-02-04 23:02:03 +0100

    fixering

--- src/Main/Entry.cs
+++ src/Main/Entry.cs
@@ -9,19 +9,31 @@ public record Entry
         this.path = path;
     }
 
-    public static Entry? From(string path) => Safe.Try<Entry, IO.DirectoryNotFoundException, IO.FileNotFoundException>(() => IsDir(path) ? Directory.From(path) : File.From(path));
+    public static Entry? From(string path) => Safe.Try<Entry, IO.DirectoryNotFoundException, IO.FileNotFoundException>(() => ExistsAs(path) switch
+    {
+        EntryType.Directory => Directory.From(path),
+        EntryType.File => File.From(path),
+        _ => null
+    });
+
     public static Entry? From(IO.FileSystemInfo info) => info.Attributes.HasFlag(IO.FileAttributes.Directory) ? Directory.From(info.FullName) : File.From(info.FullName);
 
     public virtual string Name => IO.Path.GetFileName(path);
     public string NameWithoutExtension => IO.Path.GetFileNameWithoutExtension(path);
-    public string? Extension => IO.Path.GetExtension(path)?[1..];
+    public string Extension => IO.Path.GetExtension(path).Replace(".", "");
     public string Path => path;
 
     public Directory Parent => IO.Path.GetDirectoryName(path) != null ? Directory.From(IO.Path.GetDirectoryName(path)!) : Directory.Invalid;
 
 
-    public virtual IO.FileSystemInfo? Info => Safe.Try<IO.FileSystemInfo, NullReferenceException>(() => IsDirectory ? new IO.DirectoryInfo(path) : new IO.FileInfo(path));
-    public virtual bool Exists => Safe.Try(() => IsDir(path) == false ? AsFile!.Exists : AsDirectory!.Exists);
+    public virtual IO.FileSystemInfo? Info => Safe.Try<IO.FileSystemInfo, NullReferenceException>(() => ExistsAs(path) switch
+    {
+        EntryType.Directory => new IO.DirectoryInfo(path),
+        EntryType.File => new IO.FileInfo(path),
+        _ => null
+    });
+
+    public virtual bool Exists => ExistsAs(path).HasValue;
     public virtual void Delete()
     {
         AsFile?.Delete();
@@ -31,38 +43,47 @@ public record Entry
     public Entry? Rename(Filename newName)
     {
         var newPath = IO.Path.GetDirectoryName(path) is string parent ? IO.Path.Combine(parent, newName.FilenameWithExtension) : newName.FilenameWithExtension;
-        if (IsDirectory)
+        switch (ExistsAs(path))
         {
-            IO.Directory.Move(path, newPath);
-        }
-        else
-        {
-            IO.File.Move(path, newPath);
+            case EntryType.Directory:
+                IO.Directory.Move(path, newPath);
+                break;
+
+            case EntryType.File:
+                IO.File.Move(path, newPath);
+                break;
         }
         return From(newPath);
     }
 
     public Entry? MoveTo(Directory directory)
     {
-        if (IsDirectory)
+        switch (ExistsAs(path))
         {
-            var newPath = directory / Name;
-            IO.Directory.Move(path, newPath.Path);
-            return newPath;
-        }
-        else
-        {
-            var newPath = directory / Filename.FromExtended(Name);
-            IO.File.Move(path, newPath.Path);
-            return newPath;
+            case EntryType.Directory:
+                {
+                    var newPath = directory / Name;
+                    IO.Directory.Move(path, newPath.Path);
+                    return newPath;
+                }
+
+            case EntryType.File:
+                {
+                    var newPath = directory / Filename.FromExtended(Name);
+                    IO.File.Move(path, newPath.Path);
+                    return newPath;
+                }
+
+            default:
+                return null;
         }
     }
 
-    public bool IsFile => IsDir(path) == false;
-    public bool IsDirectory => IsDir(path);
+    public bool IsFile => ExistsAs(path) == EntryType.File;
+    public bool IsDirectory => ExistsAs(path) == EntryType.Directory;
 
-    public virtual File? AsFile => IsDir(path) == false ? File.From(Path) : null;
-    public virtual Directory? AsDirectory => IsDir(path) ? Directory.From(Path) : null;
+    public virtual File? AsFile => ExistsAs(path) == EntryType.File ? File.From(Path) : null;
+    public virtual Directory? AsDirectory => ExistsAs(path) == EntryType.Directory ? Directory.From(Path) : null;
 
     public bool Visible
     {
@@ -96,6 +117,7 @@ public record Entry
 
     public override int GetHashCode() => path.GetHashCode();
 
-    static private bool IsDir(string path) => IO.File.GetAttributes(path).HasFlag(IO.FileAttributes.Directory);
+    private enum EntryType { File, Directory }
+    static private EntryType? ExistsAs(string path) => IO.Directory.Exists(path) ? EntryType.Directory : (IO.File.Exists(path) ? EntryType.File : null);
     #endregion
 }
